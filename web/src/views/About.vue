a570b0d56a9c97313b7dc57d65a5891cdfd5f529
Ann Kilzer <ann@curvegrid.com>
2020-05-25 08:39:22 +0900

    whateve

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -9,7 +9,10 @@
       Under the hood it uses <a
         href="https://uniswap.org/"
         target="_blank"
-      >Uniswap</a> and <a href="https://curvegrid.com">Curvegrid's MultiBaas</a> Platform.
+      >Uniswap</a> and <a
+        href="https://curvegrid.com"
+        target="_blank"
+      >Curvegrid's MultiBaas</a> Platform.
     </p>
   </v-container>
 </template>
